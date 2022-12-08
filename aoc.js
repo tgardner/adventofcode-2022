26de928b722b59290da1ef2a510c5300ce845332
Trent Gardner <trent.gardner@healthcareaustralia.com.au>
2022-12-08 11:45:43 +1030

    Auto download input file if not present

--- aoc.js
+++ aoc.js
@@ -1,11 +1,39 @@
+const year = 2022;
+
 const fs = require('fs');
+const https = require('https');
 const args = process.argv.slice(2);
-if(args.length == 0) {
+if (args.length == 0 || isNaN(args[0])) {
     console.log(`node aoc day part`); process.exit(1);
 }
-const puzzle = require(`./day${args[0]}.js`);
-const input = fs.readFileSync(`./day${args[0]}.txt`, 'utf-8');
-const part = args.length >=2 ? args[1] == '1' ? 1 : 2 : 1;
 
-// We'll strip out the \r depending on the filesystem we're running this from
-puzzle(input.replace(/\r/g, ""), part);
+const day = +args[0];
+const inputFile = `./day${day}.txt`;
+const sessionFile = './session.txt';
+
+const runPuzzle = () => {
+    const puzzle = require(`./day${day}.js`);
+    const input = fs.readFileSync(inputFile, 'utf-8');
+    const part = args.length >= 2 ? args[1] == '1' ? 1 : 2 : 1;
+
+    // We'll strip out the \r depending on the filesystem we're running this from
+    puzzle(input.replace(/\r/g, ""), part);
+};
+
+if (!fs.existsSync(inputFile)) {
+    console.log("Retrieving input file");
+    const session = fs.readFileSync(sessionFile);
+
+    https.request({
+        host: 'adventofcode.com',
+        path: `/${year}/day/${day}/input`,
+        headers: {
+            "Cookie": `session=${session}`
+        }
+    }, res => {
+        res.on('data', chunk => fs.writeFileSync(inputFile, chunk, { flag: 'a' }));
+        res.on('end', runPuzzle);
+    }).end();
+} else {
+    runPuzzle();
+}
--- day4.js
+++ day4.js
@@ -1,6 +1,6 @@
 const puzzle = (input, part) => {
     // 2-4,6-8
-    const lines = input.split("\n")
+    const lines = input.split("\n").filter(x => !!x.trim())
         .map(l => l.split(',').map(g => g.split('-').map(x => +x)))
         .map(l => [l[0][0], l[0][1], l[1][0], l[1][1]]);
         
