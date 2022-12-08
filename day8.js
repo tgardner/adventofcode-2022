08047ab43e0ebdaf97cc7c0b85b74a83aa59c0bd
Trent Gardner <trent.gardner@healthcareaustralia.com.au>
2022-12-09 09:17:55 +1030

    Tidy day8

--- day8.js
+++ day8.js
@@ -4,37 +4,33 @@ const puzzle = (input, part) => {
 
     const R = grid.length;
     const C = grid[0].length;
-    
-    const visible = (x, y, u, d, l, r) => {
-        if (x == 0 || x == R - 1 || y == 0 || y == C - 1)
-            return true;
-        const v = grid[x][y];
-        if (Math.max(...u) < v) return true;
-        if (Math.max(...d) < v) return true;
-        if (Math.max(...l) < v) return true;
-        if (Math.max(...r) < v) return true;
+
+    const cross = (x, y) => {
+        const up = [...Array(x).keys()].map(i => grid[i][y]).reverse();
+        const down = [...Array(R - x - 1).keys()].map(i => grid[R - 1 - i][y]).reverse();
+        const left = [...Array(y).keys()].map(i => grid[x][i]).reverse();
+        const right = [...Array(C - y - 1).keys()].map(i => grid[x][C - 1 - i]).reverse();
+        return [up, down, left, right];
+    }
+
+    const visible = (x, y) => {
+        if (x == 0 || x == R - 1 || y == 0 || y == C - 1) return true;
+        return cross(x, y).some(i => Math.max(...i) < grid[x][y]);
     };
 
-    const dist = (x, y, u, d, l, r) => {
-        const v = grid[x][y];
-
+    const dist = (x, y) => {
         const helper = (arr) => {
-            for (var i = 0; i < arr.length; ++i) if (arr[i] >= v) return i + 1;
+            for (var i = 0; i < arr.length; ++i) if (arr[i] >= grid[x][y]) return i + 1;
             return arr.length;
         };
-        return helper(u) * helper(d) * helper(l) * helper(r);
+        return cross(x, y).reduce((prod, i) => prod *= helper(i), 1);
     }
 
     let part1 = 0, part2 = 0;
     for (var i = 0; i < R; ++i) {
         for (var j = 0; j < C; ++j) {
-            const up = [...Array(i).keys()].map(x => grid[x][j]).reverse();
-            const down = [...Array(R - i - 1).keys()].map(x => grid[R - 1 - x][j]).reverse();
-            const left = [...Array(j).keys()].map(y => grid[i][y]).reverse();
-            const right = [...Array(C - j - 1).keys()].map(y => grid[i][C - 1 - y]).reverse();
-
-            if (visible(i, j, up, down, left, right)) part1++;
-            part2 = Math.max(part2, dist(i, j, up, down, left, right));
+            if (visible(i, j)) part1++;
+            part2 = Math.max(part2, dist(i, j));
         }
     }
     const result = part == 1 ? part1 : part2;
