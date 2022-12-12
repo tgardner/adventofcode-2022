6674e2d0733cc0a7ffd73b3128f93376a327ca8a
Trent Gardner <trent.gardner@gmail.com>
2022-12-12 21:00:55 +1030

    Tidy Day 12

--- day12.js
+++ day12.js
@@ -1,22 +1,17 @@
 const puzzle = (input, part) => {
+    const map = input.split("\n").filter(x => !!x.trim()).map(r => r.split(''));
     let start, end;
-    const map = input.split("\n").filter(x => !!x.trim())
-        .map((r, i) => r.split('').map((c, j) => {
-            if (c.toLowerCase() == c) {
-                return c.charCodeAt(0) - 97;
-            } else if (c == 'S') {
-                start = [i, j];
-                return 0;
-            } else {
-                end = [i, j];
-                return 25;
-            }
-        }));
+    for (let i = 0; i < map.length; ++i)
+        for (let j = 0; j < map[0].length; ++j)
+            if (map[i][j] == 'S') start = [i, j];
+            else if (map[i][j] == 'E') end = [i, j];
+
+    const height = (x) => x.toLowerCase() == x ? x.charCodeAt(0) - 97 : x == 'S' ? 0 : 25;
 
     const adj = (x, y) => [[-1, 0], [1, 0], [0, 1], [0, -1]]
         .map(d => [x + d[0], y + d[1]])
         .filter(d => d[0] >= 0 && d[0] < map.length && d[1] >= 0 && d[1] < map[0].length)
-        .filter(d => map[x][y] - map[d[0]][d[1]] <= 1);
+        .filter(d => height(map[x][y]) - height(map[d[0]][d[1]]) <= 1);
 
     const seen = new Set([end.toString()]);
     let dist = 1;
@@ -27,11 +22,8 @@ const puzzle = (input, part) => {
             for (t of adj(st[0], st[1])) {
                 if (seen.has(t.toString())) continue;
 
-                if (part == 1 && t[0] == start[0] && t[1] == start[1]) {
-                    return dist;
-                } else if (part == 2 && map[t[0]][t[1]] == 0) {
-                    return dist;
-                }
+                if (part == 1 && t[0] == start[0] && t[1] == start[1] ||
+                    part == 2 && height(map[t[0]][t[1]]) == 0) return dist;
 
                 seen.add(t.toString());
                 next.push(t);
