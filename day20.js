22af8816b1b239aa3d79f8417d82788018debe1f
Trent Gardner <trent.gardner@healthcareaustralia.com.au>
2022-12-20 16:58:26 +1030

    Tidy day 20

--- day20.js
+++ day20.js
@@ -1,8 +1,8 @@
 const puzzle = (input, part) => {
     const data = input.split("\n").filter(x => !!x.trim()).map((x, i) => [+x, i]);
-    const mixed = [...data];
-
-    const mix = (data, iter) => {
+    const solve = (data, iter) => {
+        const mixed = [...data];
+    
         for (let i = 0; i < iter; ++i) {
             for (node of data) {
                 let idx = mixed.findIndex(n => n[1] == node[1]);
@@ -17,11 +17,10 @@ const puzzle = (input, part) => {
 
         const start = mixed.findIndex(x => x[0] == 0);
         const coords = [1000, 2000, 3000].map(x => mixed[(start + x) % mixed.length][0]);
-        return coords;
+        return coords.reduce((sum, i) => sum += i, 0);
     };
     
-    const result = part == 1 ? mix(data, 1) : mix(data.map(([x, i]) => [x * 811589153, i]), 10);
-    return result.reduce((sum, i) => sum += i, 0);;
+    return part == 1 ? solve(data, 1) : solve(data.map(([x, i]) => [x * 811589153, i]), 10);
 };
 
 module.exports = puzzle;
