bb1451572f54364ba9f997a22a4d8fe0212e7234
Trent Gardner <trent.gardner@healthcareaustralia.com.au>
2022-12-20 12:19:42 +1030

    Day 19

--- day19.js
+++ day19.js
@@ -1,7 +1,5 @@
 const { ints } = require('./common.js');
 const puzzle = (input, part) => {
-    input = `Blueprint 1:    Each ore robot costs 4 ore.    Each clay robot costs 2 ore.    Each obsidian robot costs 3 ore and 14 clay.    Each geode robot costs 2 ore and 7 obsidian.
-  Blueprint 2:    Each ore robot costs 2 ore.    Each clay robot costs 3 ore.    Each obsidian robot costs 3 ore and 8 clay.    Each geode robot costs 3 ore and 12 obsidian.`
     const data = input.split("\n").filter(x => !!x.trim()).map(ints).map(([, ox, cx, obx, oby, gx, gy]) => [
         [ox, 0, 0, 0],
         [cx, 0, 0, 0],
@@ -9,13 +7,13 @@ const puzzle = (input, part) => {
         [gx, 0, gy, 0]
     ]);
 
-    const fitness = (state, mt) => {
-        const [t, res, bots] = state;
-        const geodes = res[3] + (mt - t) * bots[3];
-        return geodes * 10000000 + bots[2] * 10000 + bots[1] * 100 + bots[0];
-    }
-
     const simulate = (bp, mt) => {
+        const fitness = (state) => {
+            const [t, res, bots] = state;
+            const geodes = res[3] + (mt - t) * bots[3];
+            return geodes * 10000000 + bots[2] * 10000 + bots[1] * 100 + bots[0];
+        };
+
         let queue = [
             [0, [0, 0, 0, 0], [1, 0, 0, 0]]
         ];
@@ -27,41 +25,42 @@ const puzzle = (input, part) => {
                 cnt++;
                 let [t, res, bots] = queue.pop();
 
-                nextGen.push([t + 1, [...bots.map((b, i) => res[i] + b)], [...bots]]);
+                let next = [t + 1, [...bots.map((b, i) => res[i] + b)], [...bots]];
+                nextGen.push([...next, fitness(next)]);
 
                 for (const [b, robot] of bp.entries()) {
-                    // Can build
-                    if (robot.every((r, i) => res[i] >= r)) {
-                        let newRes = robot.map((r, i) => res[i] - r);
-                        const newBots = [...bots];
-                        newBots[b]++;
-                        newRes[b]--;
-                        newRes = newBots.map((r, i) => newRes[i] + r);
+                    if (!robot.every((r, i) => res[i] >= r)) continue;
 
-                        nextGen.push([t + 1, [...newRes], [...newBots]]);
-                    }
+                    // Can build
+                    let newRes = robot.map((r, i) => res[i] - r);
+                    const newBots = [...bots];
+                    newBots[b]++;
+                    newRes[b]--;
+                    newRes = newBots.map((r, i) => newRes[i] + r);
+
+                    next = [t + 1, [...newRes], [...newBots]];
+                    nextGen.push([...next, fitness(next)]);
                 }
             }
 
-            queue = nextGen.sort((a, b) => fitness(b, mt) - fitness(a, mt))
-                .slice(0, 20000);
+            queue = nextGen.sort(([, , , a], [, , , b]) => b - a).slice(0, 20000);
         }
 
         const [, [, , , maxGeode]] = queue.sort(([, [, , , a]], [, [, , , b]]) => b - a)[0];
         return maxGeode;
     }
 
-    const part1 = input => input
-        .map((bp, i) => [simulate(bp, 24), i + 1])
+    const part1 = (input, t) => input
+        .map((bp, i) => [simulate(bp, t), i + 1])
         .map(([a, b]) => a * b)
         .reduce((sum, v) => sum + v);
 
-    const part2 = input => input
+    const part2 = (input, t) => input
         .slice(0, 3)
-        .map(bp => harvest(bp, 32))
+        .map(bp => simulate(bp, t))
         .reduce((p, v) => p * v);
 
-    return part == 1 ? part1(data) : part2(data);
+    return part == 1 ? part1(data, 24) : part2(data, 32);
 };
 
 module.exports = puzzle;
