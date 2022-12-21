0e64bc3a4d52bade0a434041706bfee75bab4a42
Trent Gardner <trent.gardner@gmail.com>
2022-12-21 19:49:28 +1030

    Tidy Day 21

--- day21.js
+++ day21.js
@@ -1,66 +1,45 @@
 const puzzle = (input, part) => {
     const data = input.split("\n").filter(x => !!x.trim())
-    const cmds = {};
-    for (const line of data) {
-        if ((matches = line.match(/([^:]+): (.*)/))) {
-            const [, id, cmd] = matches;
+        .map(line => (matches = line.match(/([^:]+): (.*)/)) && [matches[1], matches[2]]);
+    const cmds = Object.fromEntries(data);
+
+    const solve = (root, h = null) => {
+        const queue = [root];
+        let result = root;
+        while (queue.length) {
+            const v = queue.shift()
+            const cmd = v == 'humn' && h != null ? h : cmds[v];
+            result = result.replace(v, `(${cmd})`);
             if (isNaN(cmd)) {
                 const [left, , right] = cmd.split(' ');
-                cmds[id] = [cmd, [left, right]];
-            } else {
-                cmds[id] = [cmd, []];
-            }
-        }
-    }
-
-    const part1 = (root) => {
-        const queue = [root];
-        let str = cmds[root][0];
-        while (queue.length) {
-            const [, depends] = cmds[queue.shift()];
-            for (const depend of depends) {
-                str = str.replace(depend, `(${cmds[depend][0]})`);
-                queue.push(depend);
+                queue.push(left, right);
             }
         }
 
-        return eval(str);
+        return eval(result);
     };
 
     const part2 = () => {
-        const [cmd] = cmds['root'];
-        const [rl, , rr] = cmd.split(' ');
+        const [rl, , rr] = cmds.root.split(' ');
 
-        const left = part1(rl);
-        const right = part1(rr);
+        const left = solve(rl);
+        const right = solve(rr);
+        const h = solve(rr, +cmds.humn + 1);
+        const target = h == right ? right : left;
+        const root = h == right ? rl : rr;
 
-        cmds['humn'][0]++;
-        let target = left;
-        let root = rr;
-
-        if (part1(rr) == right) {
-            target = right;
-            root = rl;
-        }
-
-        let lo = Number.MIN_SAFE_INTEGER, hi = Number.MAX_SAFE_INTEGER, mid = 0;
+        let lo = Number.MIN_SAFE_INTEGER, hi = Number.MAX_SAFE_INTEGER;
         while (lo < hi) {
-            mid = (lo + hi) / 2;
-            cmds['humn'][0] = mid;
-            score = target - part1(root);
-            if (score < 0) {
-                lo = mid;
-            } else if (score == 0) {
-                break;
-            } else {
-                hi = mid;
-            }
+            const mid = (lo + hi) / 2;
+            const score = target - solve(root, mid);
+            if (score < 0) lo = mid;
+            else if (score > 0) hi = mid;
+            else return mid;
         }
-
-        return mid;
+        return 0;
     };
 
-    return part == 1 ? part1('root') : part2();
+    return part == 1 ? solve('root') : part2();
 };
 
 module.exports = puzzle;
