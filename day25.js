2e4cf063d27db6e26c998c1f964a6792b27ff2fa
Trent Gardner <trent.gardner@gmail.com>
2022-12-26 10:57:16 +1030

    Day 25

---
+++ day25.js
@@ -0,0 +1,25 @@
+const puzzle = (input) => {
+    const lines = input.trim().split("\n").filter(x => !!x.trim());
+
+    const toDigs = {
+        0: 0,
+        1: 1,
+        2: 2,
+        '-': -1,
+        '=': -2
+    };
+    const fromDigs = Object.fromEntries(Object.entries(toDigs).map(([x, y]) => [y, x]));
+
+    const fromSnafu = s => s.split('').reduce((acc, d) => acc * 5 + toDigs[d], 0);
+    const toSnafu = n => {
+        if (!n) return "";
+        switch (n % 5) {
+            case 0: case 1: case 2: return toSnafu(Math.floor(n / 5)) + fromDigs[(n % 5).toString()]; 
+            case 3: case 4: return toSnafu(Math.floor(n / 5 + 1)) + fromDigs[(n % 5).toString() - 5];
+        }
+    };
+
+    return toSnafu(lines.map(l => fromSnafu(l)).reduce((acc, x) => acc + x, 0));
+};
+
+module.exports = puzzle;
