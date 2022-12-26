735d38a2be77946481b64c4956e9b3fb87413433
Trent Gardner <trent.gardner@gmail.com>
2022-12-26 20:42:56 +1030

    Bug fix

--- day25.js
+++ day25.js
@@ -14,8 +14,8 @@ const puzzle = (input) => {
     const toSnafu = n => {
         if (!n) return "";
         switch (n % 5) {
-            case 0: case 1: case 2: return toSnafu(Math.floor(n / 5)) + fromDigs[(n % 5).toString()]; 
-            case 3: case 4: return toSnafu(Math.floor(n / 5 + 1)) + fromDigs[(n % 5).toString() - 5];
+            case 0: case 1: case 2: return toSnafu(Math.floor(n / 5)) + fromDigs[(n % 5).toString()];
+            case 3: case 4: return toSnafu(Math.floor(n / 5 + 1)) + fromDigs[(n % 5 - 5).toString()];
         }
     };
 
